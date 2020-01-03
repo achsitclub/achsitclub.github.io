ea56836c129701448fd3b16fbe100a11d5c7e8f1
Aadarsha <connectwithadarsha@gmail.com>
2020-01-04 00:18:29 +0545

    file refactored

--- js/custom.js
+++ js/custom.js
@@ -1,53 +1,51 @@
-// 'use strict';
+'use strict';
 
-// const searchButton = document.querySelector('#searchButton')
-// const resultContainer = document.querySelector('#repo-description')
-// const starContainer = document.getElementById("repo-star")
+const searchButton = document.querySelector('#searchButton')
+const resultContainer = document.querySelector('#repo-description')
+const starContainer = document.getElementById("repo-star")
 
-// class GitHub {
-//     constructor(url) {
-//         this.url = url
-//     }
+class GitHub {
+    constructor(url) {
+        this.url = url;
+    }
 
-//     async fetchUsers() {
-//         try {
-//             const data = await axios.get(this.url)
-//             return data
-//         }   catch(err) {
-//             console.log(err)
-//         }
-//     }
+    async fetchUsers() {
+        try {
+            const data = await axios.get(this.url);
+            return data;
+        }   catch(err) {
+            console.log(err);
+        }
+    }
 
-//     async showData() {
-//         const result = await this.fetchUsers(this.url);
-//         result.data.forEach(repo => {
-//             const anchor = document.createElement('a')
-//             anchor.href = repo.html_url
-//             anchor.textContent = repo.name
-//             resultContainer.appendChild(anchor)
-//             resultContainer.appendChild(document.createElement("br"))
+    async showData() {
+        const result = await this.fetchUsers(this.url);
+        result.data.forEach(repo => {
+            const anchor = document.createElement('a');
+            anchor.href = repo.html_url;
+            anchor.textContent = repo.name;
+            resultContainer.appendChild(anchor);
+            resultContainer.appendChild(document.createElement("br"));
 
-//             resultContainer.appendChild(document.createTextNode(`${repo.description}`))
-//             resultContainer.appendChild(document.createElement("br"))
+            resultContainer.appendChild(document.createTextNode(`${repo.description}`));
+            resultContainer.appendChild(document.createElement("br"));
 
-//             resultContainer.appendChild(document.createTextNode(`Language : ${repo.language} `))
-//             resultContainer.appendChild(document.createTextNode(`Issues : ${repo.open_issues_count} `))
-//             resultContainer.appendChild(document.createTextNode(`Star : ${repo.stargazers_count} `))
-//             resultContainer.appendChild(document.createTextNode(`Forks : ${repo.forks_count} `))
-//             resultContainer.appendChild(document.createTextNode(`Updated at : ${new moment(repo.updated_at).format('MMMM Do YYYY')}`))
+            resultContainer.appendChild(document.createTextNode(`Language : ${repo.language} `));
+            resultContainer.appendChild(document.createTextNode(`Issues : ${repo.open_issues_count} `));
+            resultContainer.appendChild(document.createTextNode(`Star : ${repo.stargazers_count} `));
+            resultContainer.appendChild(document.createTextNode(`Forks : ${repo.forks_count} `));
+            resultContainer.appendChild(document.createTextNode(`Updated at : ${new moment(repo.updated_at).format('MMMM Do YYYY')}`));
 
-//             resultContainer.appendChild(document.createElement("br"))
-//             resultContainer.appendChild(document.createElement("hr"))
+            resultContainer.appendChild(document.createElement("br"));
+            resultContainer.appendChild(document.createElement("hr"));
+        });
+    }
+}
 
-//         })
-//     }
+const URL = `https://api.github.com/users/achsitclub/repos`
 
-// }
-
-// const URL = `https://api.github.com/users/achsitclub/repos`
-
-// const achsitclub = new GitHub(URL)
-// achsitclub.showData()
+const achsitclub = new GitHub(URL)
+achsitclub.showData()
 
 
 
